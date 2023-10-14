07397490832860f1e1fe5dcb4abd56ad48f1bde9
Riccardo Civiero <riccardo@riccardociviero.com>
2023-10-15 00:35:33 +0200

    hero section done

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const cursor = document.querySelector(".custom-cursor");
-
-    document.addEventListener("mousemove", (e) => {
-        cursor.style.left = e.clientX + "px";
-        cursor.style.top = e.clientY + "px";
-    });
-});
-
-
+function sendMail() {
+    window.location.href = 'mailto:riccardociviero@outlook.com';
+}
