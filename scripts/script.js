d31092c313d7d8c1e22e263a7d423fa4678e5bdd
Riccardo Civiero <riccardo@riccardociviero.com>
2023-10-23 12:10:47 +0200

    home page mobile done

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,12 @@
 function sendMail() {
     window.location.href = 'mailto:riccardociviero@outlook.com';
 }
+
+document.querySelector('.hamburger-menu').addEventListener('click', function() {
+    let menu = document.querySelector('.page-container');
+    if(menu.classList.contains('mobile-hidden')) {
+        menu.classList.remove('mobile-hidden');
+    } else {
+        menu.classList.add('mobile-hidden');
+    }
+});
